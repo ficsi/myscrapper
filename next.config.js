60ef43b03a839689bc4a00c5a036eab432c3a02e
dimata <dimitar.serafimov@egt-bg.com>
2023-11-06 15:36:37 +0200

    some fixes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	experimental: {
+		serverActions: true,
+		serverComponentsExternalPackages: ['mongoose']
+	},
+	images: {
+		domains: ['assets.coingecko.com']
+	}
+}
 
 module.exports = nextConfig
--- src/lib/actions/scrapeUserRequest.js
+++ src/lib/actions/scrapeUserRequest.js
@@ -20,3 +20,5 @@ export async function scrapeUserRequest(url) {
 		throw new Error(`Error Here: ${error.message}`)
 	}
 }
+
+
